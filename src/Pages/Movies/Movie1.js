f7cc7321314ed6b44b2031b68d5c6068dd18de36
Bishesh Shakya <specialman2099@gmail.com>
2024-05-16 08:14:44 +0545

    new

--- src/Pages/Movies/Movie1.js
+++ src/Pages/Movies/Movie1.js
@@ -3,8 +3,8 @@ const Movie1= () => {
     return(
         <div>
              <h2>Movie1:-</h2>
-             
+             {/* uidchiqweuhfiuwehf9we */}
         </div>
     )
 }
-export default Movie1;
+export default Movie1; 
